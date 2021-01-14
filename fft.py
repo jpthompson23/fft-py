dabb0a6031b57bfc7e314bca5dff9e8f87083720
John Thompson <jpthompson23@gmail.com>
2021-01-14 00:13:27 -0500

    fixing gaussian padding
    Signed-off-by: John Thompson <jpthompson23@gmail.com>

--- fft.py
+++ fft.py
@@ -33,23 +33,29 @@ def find_power_of_two(n):
             a += 1
 
 
-def make_gauss(mu, sigma):
+def make_gauss(mu, sigma, a):
     sigma_squared = sigma**2
-    return lambda x: math.exp(-0.5*(x - mu)**2 / sigma_squared)
+    return lambda x: a*math.exp(-0.5*(x - mu)**2 / sigma_squared)
 
 
-def gaussian_padding(v, fade_in=0.10):
+def gaussian_padding(v):
     n = len(v)
-    half_n = n/2.0
-    # after fade-in we should be at some number of sigma:
-    sigma = (1.0 - fade_in) * half_n / 1.5
-    gauss = make_gauss(half_n, sigma)
-    # print([gauss(i) for i in range(n)])
-    gauss_v = [(gauss(i)*value, 0) for i, value in enumerate(v)]
+
     n, remainder = find_power_of_two(n)
     pad_left = remainder // 2
     pad_right = remainder - pad_left
-    new_v = [(0, 0)]*pad_left + gauss_v + [(0, 0)]*pad_right
+
+    # sigma 10% of padding
+    sigma = pad_left/10.0
+
+    first, last = v[0], v[-1]
+    left_gauss = make_gauss(pad_left, sigma, first)
+    right_gauss = make_gauss(pad_right, sigma, last)
+    fade_in = [(left_gauss(i), 0) for i in range(pad_left)]
+    fade_out = [(right_gauss(i), 0) for i in range(pad_right)]
+    fade_out.reverse()
+
+    new_v = fade_in + [(x, 0) for x in v] + fade_out
     return new_v
 
 
@@ -57,16 +63,18 @@ def fft(v):
     v = gaussian_padding(v)
     n = len(v)
     t = _fft(v, is_forward=True)
+    a = 1.0/math.sqrt(n)
+    t = [(a*z[0], a*z[1]) for z in t]
     return v, t
 
 
 def ifft(v):
     n = len(v)
     # is_forward is False for inverse transform
-    transformed = _fft(v, is_forward=False)
-    a = 1.0/n
-    transformed = [(a*z[0], a*z[1]) for z in transformed]
-    return transformed
+    t = _fft(v, is_forward=False)
+    a = 1.0/math.sqrt(n)
+    t = [(a*z[0], a*z[1]) for z in t]
+    return t
 
 
 def _fft(v, is_forward):
@@ -85,7 +93,7 @@ def _fft(v, is_forward):
 
 
 def main():
-    signal = [cos(x*TWO_PI/32.0) for x in range(900)]
+    signal = [cos(x*TWO_PI/32.0) for x in range(1300)]
     # signal = [1.0 for x in range(1900)]
     padded_signal, t = fft(signal)
     recovered = ifft(t)
@@ -96,7 +104,7 @@ def main():
     transformed_mag = [math.sqrt(z[0]**2 + z[1]**2) for z in transformed]
     recovered_real = [z[0] for z in recovered]
     # print(transformed_mag)
-    plt.ylim(-1.0, 1.0)
+    plt.ylim(-1.5, 1.5)
     plt.plot(padded_signal)
     plt.plot(transformed_mag)
     plt.plot(recovered_real)
